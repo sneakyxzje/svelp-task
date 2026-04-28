38068bcd93ff78d644a4d258da00456ab3ba3d23
sneakyxzje <tuandattobi@gmail.com>
2026-04-28 19:16:36 +0700

    feat(infrastructure): add IUserRepository

---
+++ api/src/main/java/com/API/api/infrastructure/persistence/repository/impl/IUserRepository.java
@@ -0,0 +1,45 @@
+package com.API.api.infrastructure.persistence.repository.impl;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.API.api.domain.model.User;
+import com.API.api.domain.repository.UserRepository;
+import com.API.api.infrastructure.persistence.entity.UserEntity;
+import com.API.api.infrastructure.persistence.mapper.UserMapper;
+import com.API.api.infrastructure.persistence.repository.JUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class IUserRepository implements UserRepository {
+    private final JUserRepository jUserRepository;
+
+    public Optional<User> findByEmail(String email) {
+        Optional<UserEntity> entity = jUserRepository.findByEmail(email);
+        return entity.map(UserMapper::toDomain);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        Optional<UserEntity> entity = jUserRepository.findByUsername(username);
+        return entity.map(UserMapper::toDomain);
+    }
+
+    public boolean existsByEmail(String email) {
+        boolean entity = jUserRepository.existsByEmail(email);
+        return entity;
+    }
+
+    public boolean existsByUsername(String username) {
+        boolean entity = jUserRepository.existsByUsername(username);
+        return entity;
+    }
+
+    public User save(User user) {
+        UserEntity entity = UserMapper.toEntity(user);
+        UserEntity saved = jUserRepository.save(entity);
+        return UserMapper.toDomain(saved);
+    }
+}
