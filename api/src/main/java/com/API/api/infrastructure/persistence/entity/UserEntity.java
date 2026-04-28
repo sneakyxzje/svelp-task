05ca093d26e43ff83004bb5b9453e5d623b9f49c
sneakyxzje <tuandattobi@gmail.com>
2026-04-28 19:15:22 +0700

    feat(infrastructure): add user entity

---
+++ api/src/main/java/com/API/api/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package com.API.api.infrastructure.persistence.entity;
+
+import com.API.api.domain.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullname;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role roles;
+}
