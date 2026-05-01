2091ac5268513740621cb13df1be0492e5187e5e
sneakyxzje <tuandattobi@gmail.com>
2026-05-01 13:18:34 +0700

    feat(application): add match method for validate password

--- api/src/main/java/com/API/api/application/port/PasswordEncode.java
+++ api/src/main/java/com/API/api/application/port/PasswordEncode.java
@@ -2,4 +2,7 @@ package com.API.api.application.port;
 
 public interface PasswordEncode {
     String encode(String password);
+
+    boolean matches(String rawPassword, String encodePassword);
+
 }
