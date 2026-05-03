e0608770f658b830b7742fafd96c7a7857f062d0
sneakyxzje <tuandattobi@gmail.com>
2026-05-03 14:41:42 +0700

    feat(application): add token validation and user ID extraction methods to TokenService

--- api/src/main/java/com/API/api/application/port/TokenService.java
+++ api/src/main/java/com/API/api/application/port/TokenService.java
@@ -2,4 +2,8 @@ package com.API.api.application.port;
 
 public interface TokenService {
     String generateAccessToken(String userId, String identity);
+
+    boolean validateToken(String token);
+
+    String getUserIdFromToken(String token);
 }
