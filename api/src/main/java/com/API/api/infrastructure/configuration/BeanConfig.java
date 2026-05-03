0c68dead40abfa8b7b394e358c702b5e6357f2a1
sneakyxzje <tuandattobi@gmail.com>
2026-05-03 18:38:31 +0700

    feat(configuration): add beans for CreateWorkspaceUseCase and GetUserWorkspaceUseCase

--- api/src/main/java/com/API/api/infrastructure/configuration/BeanConfig.java
+++ api/src/main/java/com/API/api/infrastructure/configuration/BeanConfig.java
@@ -9,7 +9,10 @@ import com.API.api.application.port.PasswordEncode;
 import com.API.api.application.port.TokenService;
 import com.API.api.application.usecase.auth.LoginUseCase;
 import com.API.api.application.usecase.auth.RegisterUseCase;
+import com.API.api.application.usecase.workspace.CreateWorkspaceUseCase;
+import com.API.api.application.usecase.workspace.GetUserWorkspaceUseCase;
 import com.API.api.domain.repository.UserRepository;
+import com.API.api.domain.repository.WorkspaceRepository;
 
 @Configuration
 public class BeanConfig {
@@ -27,5 +30,15 @@ public class BeanConfig {
     @Bean
     public LoginUseCase loginUseCase(UserRepository userRepository, PasswordEncode encode, TokenService tokenService) {
         return new LoginUseCase(userRepository, encode, tokenService);
+    }
+
+    @Bean
+    public CreateWorkspaceUseCase createWorkspaceUseCase(WorkspaceRepository repo) {
+        return new CreateWorkspaceUseCase(repo);
+    }
+
+    @Bean
+    public GetUserWorkspaceUseCase getUserWorkspaceUseCase(WorkspaceRepository repo) {
+        return new GetUserWorkspaceUseCase(repo);
     }
 }
