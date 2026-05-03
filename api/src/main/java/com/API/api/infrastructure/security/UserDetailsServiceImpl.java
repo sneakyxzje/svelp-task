808e1f8a43e8ef627f0a68b832a8ea698a161a8c
sneakyxzje <tuandattobi@gmail.com>
2026-05-03 14:44:58 +0700

    feat(security): implement UserDetailsService for user authentication by username

---
+++ api/src/main/java/com/API/api/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.API.api.infrastructure.security;
+
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import com.API.api.domain.model.User;
+import com.API.api.domain.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String idOrUsername) {
+        User user = userRepository.findById(Long.parseLong(idOrUsername))
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + idOrUsername));
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(String.valueOf("ROLE_" + user.getRoles()));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), List.of(authority));
+    }
+}
