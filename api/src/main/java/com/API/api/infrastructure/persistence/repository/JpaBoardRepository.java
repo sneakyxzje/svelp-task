d5949ce18a7b99a372eda69c51fc904ced0eac92
sneakyxzje <tuandattobi@gmail.com>
2026-05-05 13:18:12 +0700

    feat(repository): create JpaBoardRepository for board data operations

---
+++ api/src/main/java/com/API/api/infrastructure/persistence/repository/JpaBoardRepository.java
@@ -0,0 +1,14 @@
+package com.API.api.infrastructure.persistence.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.API.api.infrastructure.persistence.entity.BoardEntity;
+
+public interface JpaBoardRepository extends JpaRepository<BoardEntity, Long> {
+    Optional<BoardEntity> findByName(String name);
+
+    List<BoardEntity> findAllByWorkspaceId(Long workspaceId);
+}
