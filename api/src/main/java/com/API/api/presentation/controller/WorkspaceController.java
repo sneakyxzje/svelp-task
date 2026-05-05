c9b3cf5711c5c1c8e564e688370b0814b9a59fe2
sneakyxzje <tuandattobi@gmail.com>
2026-05-05 13:18:45 +0700

    refactor(controller): remove unused UserDetails import from WorkspaceController

--- api/src/main/java/com/API/api/presentation/controller/WorkspaceController.java
+++ api/src/main/java/com/API/api/presentation/controller/WorkspaceController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
