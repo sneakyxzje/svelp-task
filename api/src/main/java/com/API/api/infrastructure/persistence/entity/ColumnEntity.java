0ef718382e5545ce09ecd8fdf85799e29761a8d4
sneakyxzje <tuandattobi@gmail.com>
2026-05-05 18:59:14 +0700

    feat(entity): create ColumnEntity class for column management

---
+++ api/src/main/java/com/API/api/infrastructure/persistence/entity/ColumnEntity.java
@@ -0,0 +1,37 @@
+package com.API.api.infrastructure.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "columns")
+@Getter
+@Setter
+public class ColumnEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id", nullable = false)
+    private BoardEntity board;
+
+    private Double position;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
