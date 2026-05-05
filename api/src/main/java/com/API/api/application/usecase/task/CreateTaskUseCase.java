cd1a07d0708a31fedb31ae5a794ead8692d74e17
sneakyxzje <tuandattobi@gmail.com>
2026-05-05 18:58:00 +0700

    feat(usecase): implement CreateTaskUseCase for creating new tasks

---
+++ api/src/main/java/com/API/api/application/usecase/task/CreateTaskUseCase.java
@@ -0,0 +1,36 @@
+package com.API.api.application.usecase.task;
+
+import java.time.LocalDateTime;
+import com.API.api.application.command.CreateTaskCommand;
+import com.API.api.application.service.ColumnPermissionService;
+import com.API.api.domain.model.Column;
+import com.API.api.domain.model.Task;
+import com.API.api.domain.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CreateTaskUseCase {
+    private final TaskRepository taskRepository;
+    private final ColumnPermissionService columnPermissionService;
+
+    public Task createTask(CreateTaskCommand command, Long userId) {
+        Column column = columnPermissionService.hasPermission(userId, command.columnId());
+
+        Double maxPos = taskRepository.findMaxPositionByColumnId(column.getId());
+        Double newPos = (maxPos == null) ? 1000.0 : maxPos + 1000.0;
+
+        Task task = new Task(
+                null,
+                command.title(),
+                command.description(),
+                column.getId(),
+                newPos,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                command.priority(),
+                command.assigneeId(),
+                command.dueDate());
+
+        return taskRepository.save(task);
+    }
+}
