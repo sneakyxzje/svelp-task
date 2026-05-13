6d237c001ed553c485b4c7e75a874b6e42f055ff
sneakyxzje <tuandattobi@gmail.com>
2026-05-13 15:08:10 +0700

    refactor(layout): remove console logs

--- www/src/routes/(private)/+layout.ts
+++ www/src/routes/(private)/+layout.ts
@@ -3,8 +3,6 @@ import type { Workspace } from '$lib/interface/workspace.js';
 
 export const load = async ({ fetch }) => {
 	const response = await http(fetch).get<Workspace[]>('/workspaces');
-	console.log('API Response Data:', response.data);
-	console.log('API Response Error:', response.error);
 	if (response.data) {
 		return {
 			workspaces: response.data
